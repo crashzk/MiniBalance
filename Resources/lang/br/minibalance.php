934251a7329a13c673f5ddd2810afce2301f348d
crashzk <crashzk@zkservidores.com>
2026-03-28 08:17:35 -0300

    Create minibalance.php

---
+++ Resources/lang/br/minibalance.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'module_name' => 'MiniBalance',
+    'description' => 'Crie um mini widget para exibir o saldo',
+    'balance' => 'Saldo',
+    'topup' => 'Recarregar saldo',
+];
